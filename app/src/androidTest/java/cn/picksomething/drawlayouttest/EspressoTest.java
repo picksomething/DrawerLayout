c8799ff75a2fbbde70d165d878af078819f54c30
picksomething <zzucaobin@gmail.com>
2016-04-18 14:41:16 +0800

    Add test case to test NavigationView

---
+++ app/src/androidTest/java/cn/picksomething/drawlayouttest/EspressoTest.java
@@ -0,0 +1,108 @@
+package cn.picksomething.drawlayouttest;
+
+import android.app.Activity;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.action.ViewActions.click;
+
+
+/**
+ * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
+ * {@link ViewActions#click} and {@link ViewActions#typeText}.
+ * <p/>
+ * Note that there is no need to tell Espresso that a view is in a different {@link Activity}.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class EspressoTest {
+
+
+    /**
+     * A JUnit {@link Rule @Rule} to launch your activity under test. This is a replacement
+     * for {@link ActivityInstrumentationTestCase2}.
+     * <p/>
+     * Rules are interceptors which are executed for each test method and will run before
+     * any of your setup code in the {@link Before @Before} method.
+     * <p/>
+     * {@link ActivityTestRule} will create and launch of the activity for you and also expose
+     * the activity under test. To get a reference to the activity you can use
+     * the {@link ActivityTestRule#getActivity()} method.
+     */
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withText("从左边边界右滑或者点击左上角导航图标打开抽屉布局")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void openNavigationDrawer() {
+        onView(withId(R.id.drawer_layout)).perform(actionOpenDrawer());
+    }
+
+    /*@Test
+    public void clickDrawerItems(){
+        onView(withText("地球")).perform(click());
+    }*/
+
+    private ViewAction actionOpenDrawer() {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(DrawerLayout.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "open drawer";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((DrawerLayout) view).openDrawer(GravityCompat.START);
+            }
+        };
+    }
+
+    private ViewAction actionCloseDrawer() {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isAssignableFrom(DrawerLayout.class);
+            }
+
+            @Override
+            public String getDescription() {
+                return "close drawer";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((DrawerLayout) view).closeDrawer(GravityCompat.START);
+            }
+        };
+    }
+}
